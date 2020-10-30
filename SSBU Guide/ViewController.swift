435fb43e106ddf53587d47b92b9b7cd1dde950b4
adeluca63 <aidanjohndeluca@gmail.com>
2020-10-30 09:46:26 -0400

    Main Code

--- SSBU Guide/ViewController.swift
+++ SSBU Guide/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var myIVM: UIImageView!
+    @IBOutlet weak var myIVDK: UIImageView!
+    @IBOutlet weak var myIVL: UIImageView!
+    @IBOutlet weak var myIVS: UIImageView!
+    @IBOutlet weak var myIVY: UIImageView!
+    @IBOutlet weak var myIVK: UIImageView!
+    @IBOutlet weak var myIVF: UIImageView!
+    @IBOutlet weak var myIVP: UIImageView!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myIVM.image = UIImage(named: "Mario")
+        myIVDK.image = UIImage(named: "DK")
+        myIVL.image = UIImage(named: "Link")
+        myIVS.image = UIImage(named: "Samus")
+        myIVY.image = UIImage(named: "Yoshi")
+        myIVK.image = UIImage(named: "Kirby")
+        myIVF.image = UIImage(named: "Fox")
+        myIVP.image = UIImage(named: "Pikachu")
     }
 
 
